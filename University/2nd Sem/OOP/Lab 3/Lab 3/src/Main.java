b58297e7217cb796881eaec6a422567d88045d25
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-08-25 15:56:26 +0500

    modified:   University/2nd Sem/OOP/Lab 3/Lab 3/bin/Main.class 	new file:   University/2nd Sem/OOP/Lab 3/Lab 3/bin/MinMax.class 	modified:   University/2nd Sem/OOP/Lab 3/Lab 3/src/Main.java 	new file:   University/2nd Sem/OOP/Lab 3/Lab 3/src/MinMax.java

--- University/2nd Sem/OOP/Lab 3/Lab 3/src/Main.java
+++ University/2nd Sem/OOP/Lab 3/Lab 3/src/Main.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
 public class Main {
-
+    public static void main(String[] args)
+    {
+        Scanner inp = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.println("Enter array elements from here on out.");
+        for(int i=0;i<10;i++)
+        {
+            arr[i] = inp.nextInt();
+        }
+        System.out.println("Maximum Element of the array is: "+MinMax.max(arr));
+        System.out.println("Minimum Element of the array is: "+MinMax.min(arr));
+    }
 }
---
+++ University/2nd Sem/OOP/Lab 3/Lab 3/src/MinMax.java
@@ -0,0 +1,20 @@
+public class MinMax {
+    public static int min(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min; 
+    }
+    public static int max(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max; 
+    }
+}
