0994d6502ba98103cc4b204dfa9b8ddb69b02d6a
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-08-25 15:44:09 +0500

    new file:   University/2nd Sem/OOP/Lab 3/Lab 3/bin/Main.class modified:   University/2nd Sem/OOP/Lab 3/Lab 3/bin/SearchArr.class new file:   University/2nd Sem/OOP/Lab 3/Lab 3/src/Main.java modified:   University/2nd Sem/OOP/Lab 3/Lab 3/src/SearchArr.java

--- University/2nd Sem/OOP/Lab 3/Lab 3/src/SearchArr.java
+++ University/2nd Sem/OOP/Lab 3/Lab 3/src/SearchArr.java
@@ -1,3 +1,24 @@
 public class SearchArr {
-
+    public static boolean searchNum(int[] arr,int a) {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==a)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,};
+        int a = 3;
+        if(searchNum(arr, a))
+        {
+            System.out.println("Element is present in array");
+        }
+        else
+        {
+            System.out.println("Element is not present in array");
+        }
+    }
 }
