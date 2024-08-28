9e4a481bc8780906a609966b502e402716643f2b
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-08-28 14:53:26 +0500

    new file:   DSA- Self Paced/CPP/Pattern Questions - Loop/Hollow Rectangle.cpp 	new file:   DSA- Self Paced/CPP/Pattern Questions - Loop/Hollow Rectangle.exe

---
+++ DSA- Self Paced/CPP/Pattern Questions - Loop/Hollow Rectangle.cpp
@@ -0,0 +1,30 @@
+/*Hollow Rectangle
+*****
+*   *
+*   *
+***** 
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int rows,cols;
+	cout<<"Enter number of rows and coloumns: ";
+	cin>>rows>>cols;
+	for(int i=1;i<=rows;i++)
+	{
+		for(int j=1;j<=cols;j++)
+		{
+			if(i==1||i==rows||j==1||j==cols)
+			{
+				cout<<"*";
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
