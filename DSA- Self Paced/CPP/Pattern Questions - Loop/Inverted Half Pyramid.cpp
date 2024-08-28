89e807b1b352ed413bb5d5bc0befe990774f9531
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-08-28 22:43:47 +0500

    new file:   DSA- Self Paced/CPP/Pattern Questions - Loop/Inverted Half Pyramid.cpp 	new file:   DSA- Self Paced/CPP/Pattern Questions - Loop/Inverted Half Pyramid.exe

---
+++ DSA- Self Paced/CPP/Pattern Questions - Loop/Inverted Half Pyramid.cpp
@@ -0,0 +1,25 @@
+/*Inverted Half Pyramid
+******
+*****
+****
+***
+**
+*
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int row, col;
+	cout<<"Enter the number of rows and cols: ";
+	cin>>row>>col;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=col-i;j>0;j--)
+		{
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
