6fd48f562093d385c3d2c2a70135c438e40d6f3b
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-10-05 17:35:32 +0500

    deleted:    University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem 3.cpp 	new file:   University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem 3.exe 	renamed:    University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem 1.cpp -> University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_1.cpp 	new file:   University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_1.exe 	renamed:    University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem 2.cpp -> University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_2.cpp 	new file:   University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_2.exe 	new file:   University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_3.cpp 	new file:   University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_3.exe

---
+++ University/3rd Sem/DSA/Labs/Lab 1- 01.Oct.2024/Problem_3.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string upperCase(string);
+string lowerCase(string);
+string titleCase(string);
+int main()
+{
+	string name;
+	cout<<"Enter your name: ";
+	getline(cin,name);
+	cout<<"Your name in upper case is: "<<upperCase(name)<<endl;
+	cout<<"Your name in lower case is: "<<lowerCase(name)<<endl;
+	cout<<"Your name in title case is: "<<titleCase(name)<<endl;
+	return 0;
+}
+
+string upperCase(string name)
+{
+	int length =  name.length();
+	for(int i=0;i<length;i++)
+	{
+		char c = name[i];
+		name[i] = toupper(c);
+	}
+	return name;
+}
+
+string lowerCase(string name)
+{
+	int length =  name.length();
+	for(int i=0;i<length;i++)
+	{
+		char c = name[i];
+		name[i] = tolower(c);
+	}
+	return name;
+}
+
+string titleCase(string name)
+{
+	int length =  name.length();
+	for(int i=0;i<length;i++)
+	{
+		int index = 0;
+		char c = name[i];
+		if(i==0 && c!=' ')
+		{
+			if(c>='a' && c<='z')
+			{
+				name[i] = toupper(c);
+			}
+		}
+		else if(i=index)
+		{
+			if(name[i]!=' '&& (c>='a' && c<='z'))
+			{
+				name[i] = toupper(c);
+			}
+		}
+		else
+		{
+			if(c>='A' && c<='Z')
+			{
+				name[i] = tolower(c);
+			}
+			else if(c==' ')
+			{
+				index = i+1;
+			}
+		}
+	}
+	return name;
+}
