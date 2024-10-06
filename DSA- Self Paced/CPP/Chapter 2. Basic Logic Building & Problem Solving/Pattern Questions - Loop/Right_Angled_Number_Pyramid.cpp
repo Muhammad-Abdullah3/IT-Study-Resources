d3ce5b944165eaa2b48b09c49121610c4545cf9f
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2024-10-06 14:51:39 +0500

    new file:   DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Number_Pyramid.cpp 	new file:   DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Number_Pyramid.exe 	modified:   DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Triangle.cpp 	new file:   DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Triangle.exe

--- DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Number_Pyramid.cpp
+++ DSA- Self Paced/CPP/Chapter 2. Basic Logic Building & Problem Solving/Pattern Questions - Loop/Right_Angled_Number_Pyramid.cpp
@@ -1,16 +1,16 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int rows = 0;
+    int n;
 	cout<<"Enter the number of Rows you want to Print: ";
-	cin>>rows;
-	for(int i=0;i<rows;i++)
+	cin>>n;
+	for(int i=1;i<=n;i++)
 	{
-		for(int j=0;j<=i;j++)
+		for(int j=1;j<=i;j++)
 		{
-			cout<<"*";
+			cout<<j;
 		}
 		cout<<endl;
 	}
-    return 0;
+	return 0;
 }
