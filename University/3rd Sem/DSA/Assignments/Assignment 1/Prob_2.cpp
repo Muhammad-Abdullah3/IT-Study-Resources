19bf103ea977a6ecb6fcaa1e4bd4690c4f8970f4
Muhammad Abdullah <maabdullah.rhs@gmail.com>
2024-10-20 04:38:53 +0500

    new file:   .vscode/settings.json 	new file:   University/3rd Sem/DSA/Assignments/Assignment 1/Assignment 1 -  Abdullah (53457) - DSA.pdf 	modified:   University/3rd Sem/DSA/Assignments/Assignment 1/Prob_2.cpp 	new file:   University/3rd Sem/DSA/Assignments/Assignment 1/Prob_2.exe 	new file:   University/3rd Sem/DSA/Assignments/Assignment 1/~$A Assignment 1.docx

--- University/3rd Sem/DSA/Assignments/Assignment 1/Prob_2.cpp
+++ University/3rd Sem/DSA/Assignments/Assignment 1/Prob_2.cpp
@@ -2,3 +2,77 @@
 #include<vector>
 #include<string>
 using namespace std;
+
+class Shape {
+    double getArea() {
+        return 0;
+    }
+};
+
+class Circle: public Shape {
+    private:
+    double radius;
+    public:
+    void setPerimeter() {
+        double rad;
+        cout<<"Enter Radius for Circle: ";
+        cin>>rad;
+        radius = rad;
+    }
+    double getArea() {
+    return 3.14*radius*radius;
+    }
+};
+
+class Rectangle: public Shape {
+    private:
+    double length, width;
+    public:
+    void setPerimeter() {
+        double len;
+        cout<<"Enter Length for Rectangle: ";
+        cin>>len;
+        length = len;
+        double wid;
+        cout<<"Enter Width for Rectangle: ";
+        cin>>wid;
+        width = wid;
+    }
+    double getArea() {
+        return (double) length*width;
+    }
+};
+
+int main() {
+    //vector<Shape> shapes;
+    vector<Circle> circles;
+    vector<Rectangle> rectangles;
+    int n;
+    cout<<"Enter number of shapes to find area of: ";
+    cin>>n;
+    for(int i = 0; i < n; i++) {
+        int choice;
+        cout<<"Enter even for Circle, odd for Rectangle: ";
+        cin>>choice;
+        //shapes.push_back(choice%2==0 ? Circle():Rectangle());
+        if(choice%2==0) {
+            circles.push_back(Circle());
+            circles[i].setPerimeter();
+            cout<<"Area of Circle "<<i+1<<": "<<circles[i].getArea()<<endl;
+        }
+        else {
+            rectangles.push_back(Rectangle());
+            rectangles[i].setPerimeter();
+            cout<<"Area of Rectangle "<<i+1<<": "<<rectangles[i].getArea()<<endl;
+        }
+    }
+    /*cout<<"\n\t\t\t\tArea of Circles: \t\t\t\t\n";
+    for(int i=0;i<circles.size();i++) {
+        cout<<"Area of Circle "<<i+1<<": "<<circles[i].getArea()<<endl;
+    }
+    cout<<"\n\t\t\t\tArea of Rectangles: \t\t\t\t\n";
+    for(int i=0;i<rectangles.size();i++) {
+        cout<<"Area of Rectangle "<<i+1<<": "<<rectangles[i].getArea()<<endl;
+    }*/
+    return 0;
+}
