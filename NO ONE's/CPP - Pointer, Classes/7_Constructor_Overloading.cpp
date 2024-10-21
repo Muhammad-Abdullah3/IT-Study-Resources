301b5676d8ab20eb545f61b5d57ce0d3018c5a4f
Muhammad Abdullah <maabdullah.rhs@gmail.com>
2024-10-21 12:52:42 +0500

    new file:   NO ONE's/CPP - Pointer, Classes/7_Constructor_Overloading.cpp 	new file:   NO ONE's/CPP - Pointer, Classes/8_Dynamic_Initialization_of_Objects_using_Constructors.cpp

---
+++ NO ONE's/CPP - Pointer, Classes/7_Constructor_Overloading.cpp
@@ -0,0 +1,3 @@
+// ye khud dekh len dono way se karlen jo previous 2 programs mein constructors diye gaye hain
+// us type ke constructor and phir unko overload karke practice karen 
+// dono ways se
---
+++ NO ONE's/CPP - Pointer, Classes/8_Dynamic_Initialization_of_Objects_using_Constructors.cpp
@@ -0,0 +1,2 @@
+// is program mein ham dekhenge ke aik object ko ham dynamically kese initialize kar 
+// sakte hain using constructors
