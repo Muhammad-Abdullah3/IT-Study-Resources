7760005adfc4bbfcc1d240df5ef430a60c575e4c
Muhammad Abdullah <maabdullah.rhs@gmail.com>
2024-11-11 21:50:28 +0500

    modified:   University/3rd Sem/DSA/Lectures/Week 6/BubbleSort.cpp

--- University/3rd Sem/DSA/Lectures/Week 6/BubbleSort.cpp
+++ University/3rd Sem/DSA/Lectures/Week 6/BubbleSort.cpp
@@ -1,1 +1,18 @@
-#include<iostream
+#include<iostream> 
+#include<vector>
+using namespace std;
+void bubbleSort(vector<int> vect) {
+    for(int i=0;i<vect.size();i++) {
+        for(int j=0;j<vect.size()-i-1;j++) {
+            if(vect[i]>vect[j]) {
+                int temp = vect[i];
+                vect[i] = vect[j];
+                vect[j] = vect[i];
+            }
+        }
+    }
+}
+
+int main() {
+    
+}
